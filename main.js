0d6b06099bd26c35d5e6759bbdd66325c23b00f1
mbvDevelop <mbv.design.co@gmail.com>
2021-11-17 22:46:14 +0100

    Iteracion 8

--- main.js
+++ main.js
@@ -1,35 +1,19 @@
-// Iteracion 7
+// Iteracion 8
  
-//  7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
-// los alumnos usando la función .reduce().
+// 6.1 Dado el siguiente javascript filtra los videojuegos por gender = 'RPG' usando 
+// .filter() y usa .reduce() para conseguir la media de sus .score. 
+// La función .find() también podría ayudarte para el contrar el genero 'RPG' en el 
+// array .gender.
  
-const exams = [
-    {name: 'Yuyu Cabeza Crack', score: 5}, 
-    {name: 'Maria Aranda Jimenez', score: 1}, 
-    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
-    {name: 'Mercedez Regrera Brito', score: 7},
-    {name: 'Pamela Anderson', score: 3},
-    {name: 'Enrique Perez Lijó', score: 6},
-    {name: 'Pedro Benitez Pacheco', score: 8},
-    {name: 'Ayumi Hamasaki', score: 4},
-    {name: 'Robert Kiyosaki', score: 2},
-    {name: 'Keanu Reeves', score: 10}
+const videogames = [
+    {name: 'Final Fantasy VII', genders: ['RPG'], score: 9.5},
+    {name: 'Assasins Creed Valhala', genders: ['Aventura', 'RPG'], score: 4.5},
+    {name: 'The last of Us 2', genders: ['Acción', 'Aventura'], score: 9.8},
+    {name: 'Super Mario Bros', genders: ['Plataforma'], score: 8.5},
+    {name: 'Genshin Impact', genders: ['RPG', 'Aventura'], score: 7.5},
+    {name: 'Legend of Zelda: Breath of the wild', genders: ['RPG', 'La cosa más puto bonita que he visto nunca'], score: 10},
 ]
-const examTotal = exams.reduce((accumulator,  exam) => accumulator + exam.score,  0)
-console.log(examTotal)
  
-// 7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
-// alumnos que esten aprobados usando la función .reduce().
-const examApproved = exams
-.filter(exam => exam.score > 4)
-.reduce((accumulator, exam) => {
-  return accumulator + exam.score
-},  0)
-console.log(examApproved)
- 
-// 7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
-const  examsAvg =  exams.reduce((acc, exam) => {
-  return acc  + exam.score / exams.length
-}, 0) 
- 
-console.log(examsAvg)
+let rpgGames = videogames.filter(game => game.genders.includes('RPG'))
+let gamesScore = rpgGames.reduce((acc, game) => acc + game.score / rpgGames.length, 0)
+console.log(gamesScore)
