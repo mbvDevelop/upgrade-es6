7182148f151293c377f6db358837fc9e548cc591
mbvDevelop <mbv.design.co@gmail.com>
2021-11-17 22:37:38 +0100

    Iteracion 2

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-// Crea una arrow function que tenga dos parametros a y b y 
-// que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
-// por consola la suma de los dos parametros.
-const func = (a = 10, b = 5) => a + b
-// 1.1 Ejecuta esta función sin pasar ningún parametro
-console.log(func())
-// 1.2 Ejecuta esta función pasando un solo parametro
-console.log(func(15))
-// 1.3 Ejecuta esta función pasando dos parametros
-console.log(func(15, 30))
+// iteracion dos
 
+// 2.1 En base al siguiente javascript, crea variables en base a las propiedades 
+// del objeto usando object destructuring e imprimelas por consola. Cuidado, 
+// no hace falta hacer destructuring del array, solo del objeto.
+
+const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
+let {title, gender, year} = game
+console.log(title)
+console.log(gender)
+console.log(year)
+
+// 2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
+// llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
+// imprimelo por consola.
+
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+let [fruit1, fruit2, fruit3] = fruits
+console.log(fruit2)
+
+// 2.3 En base al siguiente javascript, usa destructuring para crear 2 
+// variables igualandolo a la función e imprimiendolo por consola.
+
+const animalFunction = () => {
+    return {name: 'Bengal Tiger', race: 'Tiger'}
+};
+
+let {name, race} = animalFunction()
+console.log(race)
+
+// 2.4 En base al siguiente javascript, usa destructuring para crear las 
+// variables name y itv con sus respectivos valores. Posteriormente crea 
+// 3 variables usando igualmente el destructuring para cada uno de los años 
+// y comprueba que todo esta bien imprimiendolo.
+
+const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+let {name, itv} = car
+let [year1, year2, year3] = itv
+console.log(year2)
