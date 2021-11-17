a3b1b49d6f675bdb815dbc4d94d0710fc4833111
mbvDevelop <mbv.design.co@gmail.com>
2021-11-17 22:44:55 +0100

    Iteracion 5

--- main.js
+++ main.js
@@ -1,48 +1,84 @@
-// Iteración 4
- 
-// 4.1 Dado el siguiente array, devuelve un array con sus nombres 
-// utilizando .map().
-const users = [
- {id: 1, name: 'Abel'},
- {id:2, name: 'Julia'},
- {id:3, name: 'Pedro'},
- {id:4, name: 'Amanda'}
+// 5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean mayor que 18
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+const agesOver18 = ages.filter(age => age > 18)
+console.log(agesOver18)
+
+// 5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean par.
+const ages1 = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+const ages1Pair = ages.filter(age => age % 2 == 0)
+console.log(ages1Pair)
+
+// 5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+const streamers = [
+ {name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+ {name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+ {name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+ {name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
 ];
- 
-let usersNames = users.map(user => user.name)
-console.log(usersNames)
- 
-// 4.2 Dado el siguiente array, devuelve una lista que contenga los valores 
-// de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que 
-// empiece por 'A'.
-const users1 = [
- {id: 1, name: 'Abel'},
- {id:2, name: 'Julia'},
- {id:3, name: 'Pedro'},
- {id:4, name: 'Amanda'}
+const streamersLeage = streamers.filter(streamer => streamer.gameMorePlayed === "League of Legends")
+console.log(streamersLeage)
+
+// 5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos 
+// usar la funcion .includes() para la comprobación.
+const streamers1 = [
+ {name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+ {name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+ {name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+ {name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
 ];
-let usersNames1 = users1.map(function(user) {
-  if (user.name.charAt(0) == ('a') || user.name.charAt(0) == "A"){
-    return "Anacleto"
+const streamersWithA = streamers1.map(streamer => streamer.name.includes("a"))
+
+// 5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+// el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
+// .includes() para la comprobación.
+// Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
+// .age sea mayor que 35.
+const streamersWTH = streamers1.filter(streamer => streamer.gameMorePlayed.includes('Legends')).map(streamer => {
+  if (streamer.age > 35) {
+    streamer.gameMorePlayed = streamer.gameMorePlayed.toUpperCase()
   }
-  return user.name
+  return streamer
 })
-console.log(usersNames1)
-// 4.3 Dado el siguiente array, devuelve una lista que contenga los valores 
-// de la propiedad .name y añade al valor de .name el string ' (Visitado)' 
-// cuando el valor de la propiedad isVisited = true.
-const cities = [
- {isVisited:true, name: 'Tokyo'}, 
- {isVisited:false, name: 'Madagascar'},
- {isVisited:true, name: 'Amsterdam'}, 
- {isVisited:false, name: 'Seul'}
+console.log(streamersWTH)
+ 
+// 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+// introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+const streamers3 = [
+ {name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+ {name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+ {name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+ {name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
 ];
+const input = document.querySelector("input")
+const inputed = () =>  {
+  let text = input.value
+  let streamer = streamers3.filter( streamer => streamer.name.includes(text))
+  console.log(streamer)
+}
+input.addEventListener("input", inputed)
  
-let cities2 = cities.map(function(city) {
-  if (city.isVisited) {
-    return city.name + "(Visitado)"
-  }
-  return city.name
-})
- 
-console.log(cities2)
+// 5.7 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si introduzco 'i', 
+// me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+// En este caso, muestra solo los streamers filtrados cuando hagamos click en el button.
+const streamers4 = [
+ {name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+ {name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+ {name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+ {name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+const input1 = document.querySelector("input")
+const button = document.querySelector("button")
+const inputed1 = () =>  {
+  let text = input.value
+  let streamer = streamers5.filter( streamer => streamer.name.includes(text))
+  console.log(streamer)
+}
+button.addEventListener("click", inputed)
